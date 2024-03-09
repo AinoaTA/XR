73adb1b26c0d94dadf6b8ce0ef7c96bc8c22f8d4
Ainoa Tabares Amaya <61891371+AinoaTA@users.noreply.github.com>
2024-03-09 15:09:19 +0100

    helmet

--- Assets/Scripts/Interactions/Helmet.cs
+++ Assets/Scripts/Interactions/Helmet.cs
@@ -1,43 +1,69 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Interctable
 {
     public class Helmet : MonoBehaviour, IInteractable
     {
+        [SerializeField] private ParticleIndicator _particle;
+
         private Vector3 _initPos;
         private Quaternion _initRot;
+        private bool _canInteract; 
+        private XRBaseInteractable _interactable;
 
-        private bool _canInteract;
+        private void OnEnable()
+        {
+            if (_interactable == null)
+                TryGetComponent(out _interactable);
 
-        [SerializeField] private ParticleIndicator _particle;
+            _interactable.selectEntered.AddListener(Interacting);
+            _interactable.selectExited.AddListener(Deselect);
+        }
+        private void OnDisable()
+        {
+            _interactable.selectEntered.RemoveAllListeners();
+            _interactable.selectExited.RemoveAllListeners();
+        }
 
         private void Awake()
         {
             _initPos = transform.position;
-            _initRot = transform.rotation;
+            _initRot = transform.rotation; 
         }
 
         private void Start()
         {
             _canInteract = true;
 
-            if (_canInteract)
-                _particle.StartIndicator();
+            if (_canInteract) _particle.StartIndicator();
         }
 
         public void Interact()
         {
             if (!_canInteract) return;
-            _canInteract = false;
+            _canInteract = false; 
+
             _particle.StopIndicator();
         }
 
-        private void ResetAction()
+        private void Interacting(BaseInteractionEventArgs e)
         {
-            transform.position = _initPos;
-            transform.rotation = _initRot;
-            _canInteract = true;
-            _particle.StartIndicator();
+            Interact();
         }
+
+        private void Deselect(BaseInteractionEventArgs e)
+        {
+            ResetAction();
+        }
+
+        private void ResetAction()
+        {  
+            transform.SetPositionAndRotation(_initPos, _initRot);
+
+            _particle.StartIndicator();
+            
+            _canInteract = true;
+        } 
     }
 }
